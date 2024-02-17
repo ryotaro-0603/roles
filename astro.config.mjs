caeb5f1c1ff856510ea848d0e9c7952f845d3cb1
ryotaro0603 <ryotaro.vvv@gmail.com>
2024-02-18 04:20:35 +0900

    vercel setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   i18n: {
     defaultLocale: "fr",
-    locales: ["fr", "pt-br", "es"],
+    locales: ["fr", "pt-br", "es"]
   },
+  output: "server",
+  adapter: vercel()
 });
