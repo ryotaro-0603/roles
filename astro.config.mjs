902d3c461b3c3ca51949099daaa3bf753d2534d1
ryotaro0603 <ryotaro.vvv@gmail.com>
2024-02-18 20:09:25 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  i18n: {
-    defaultLocale: "fr",
-    locales: ["fr", "pt-br", "es"]
-  },
-  output: "server",
-  adapter: vercel(),
-  compressHTML: false,
 });
