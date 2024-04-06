75ea25b5fb6a489f10621d4ee91770ade94eaac2
ryotaro0603 <ryotaro.vvv@gmail.com>
2024-04-06 18:41:06 +0900

    fix

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,2 @@
-module.exports = {
-  plugins: ['prettier-plugin-tailwindcss'],
-  tailwindConfig: './styles/tailwind.config.js',
-};
+export const plugins = ['prettier-plugin-tailwindcss'];
+export const tailwindConfig = './styles/tailwind.config.js';
---
+++ src/scripts/recaptcha.js
@@ -0,0 +1,6 @@
+const onSubmit = (token) => {
+  const form = document.getElementById('sample-form');
+  if (form.reportValidity()) {
+    form.submit();
+  }
+};
