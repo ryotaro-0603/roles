572e1c4880441df4d7ca852398faed4b995c741e
ryotaro0603 <ryotaro.vvv@gmail.com>
2024-04-06 18:49:29 +0900

    fix

--- src/scripts/recaptcha.js
+++ src/scripts/recaptcha.js
@@ -1,6 +1,6 @@
-const onSubmit = (token) => {
+export default function onSubmit(token) {
   const form = document.getElementById('sample-form');
   if (form.reportValidity()) {
     form.submit();
   }
-};
+}
